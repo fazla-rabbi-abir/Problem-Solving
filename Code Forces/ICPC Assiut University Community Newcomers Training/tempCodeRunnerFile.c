722f981c08e2abb0d34ccfecc2ddcdc58e4c5e21
Md Fazla Rabbi Abir <131839058+fazla-rabbi-abir@users.noreply.github.com>
2025-05-04 12:22:49 +0600

    Add files via upload

---
+++ Code Forces/ICPC Assiut University Community Newcomers Training/tempCodeRunnerFile.c
@@ -0,0 +1,14 @@
+        if(x > 0)
+        {
+            pos++;
+        }
+        else if(x < 0)
+        {
+            neg++;
+        }
+    }
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", pos);
+    printf("Negative: %d\n", neg);
+}
