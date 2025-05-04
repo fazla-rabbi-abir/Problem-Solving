39d9427b0217f404d72e4c685cde5e4db79f3f07
Md Fazla Rabbi Abir <131839058+fazla-rabbi-abir@users.noreply.github.com>
2025-05-04 12:21:34 +0600

    Add files via upload

---
+++ Code Forces/ICPC Assiut University Community Newcomers Training/A_Summation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    for(int i=0; i<N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    int sum = 0;
+    for(int i=0; i<N; i++)
+    {
+        sum += A[i];
+    }
+
+    printf("%d", sum);
+}
